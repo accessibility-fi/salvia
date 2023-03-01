40981a27897cfa0aed7f962b5635d212d94b3772
Olga Nippolainen <olga.nippolainen@tietoevry.com>
2023-03-01 10:22:20 +0200

    remove unused package

--- app/salvia-ui/src/i18n.js
+++ app/salvia-ui/src/i18n.js
@@ -1,14 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from "i18next-browser-languagedetector";
-import XHR from "i18next-xhr-backend";
-
 import translationEN from "./locales/en.json";
 import translationFI from "./locales/fi.json";
 import translationSV from "./locales/sv.json";
 
 i18n
-  .use(XHR)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
